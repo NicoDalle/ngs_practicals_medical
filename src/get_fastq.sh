f43070a65960c8d87643a1fdc6a7a7c25d2e4c72
Nicolas Dalle <nicolas.dalle@ens-lyon.fr>
2019-11-05 15:58:32 +0000

    Téléchargement OK

--- src/get_fastq.sh
+++ src/get_fastq.sh
@@ -1,23 +1,22 @@
 #! /bin/bash
 
 # Hsapiens
-data="/home/rstudio/TP_RNAseq_Medical"
+data="/home/rstudio/disk"
 mkdir -p $data
 cd $data
 mkdir -p sra_data
 cd sra_data
 
-SRR="SRR3308950
-SRR3308951
-SRR3308952
+#Liste des SRR d'intérêt
+SRR="SRR3308952
+SRR3308954
+SRR3308956
+SRR3308963
 "
 
 for srr in $SRR
 do
-# Produces one fastq file, single end data. 
-fastq-dump -h
+# Télécharge les séquences correspondant aux SRR renseignés avant
+fastq-dump $srr -O /home/rstudio/disk/sra_data -X 100000
 
-# rename sequence names
-awk  -F "\."  '{ if (NR%2 == 1 ) { $3= "" ; print $1 "_" $2 "/1"}  else  { print $0} }'
-...
 done
