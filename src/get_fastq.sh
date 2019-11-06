98eee87e58445a3901a855e6c691ac6ca457810f
Nicolas Dalle <nicolas.dalle@ens-yon.fr>
2019-11-06 09:09:02 +0000

    Téléchargement et renommage des données fatsq

--- src/get_fastq.sh
+++ src/get_fastq.sh
@@ -16,7 +16,17 @@ SRR3308963
 
 for srr in $SRR
 do
-# Télécharge les séquences correspondant aux SRR renseignés avant
-fastq-dump $srr -O /home/rstudio/disk/sra_data -X 100000
+#Télécharge les séquences correspondant aux SRR renseignés avant
+#split-file : séquençage pair-end, les séquences dans les 2 sens sont dans 2 fichiers différents
+fastq-dump $srr -O /home/rstudio/disk/sra_data --split-files -I
+
+#Modifie les fichiers texte, NR: row number
+#Change le nom des séquences en SRR..._1/1 et enlève la taille du read
+awk -F"\." '{ if (NR%2 == 1) { $3="" ; print $1 "_" $2 "/1"} else {print $0} }' $srr'_1.fastq' > temp1.fastq
+awk -F"\." '{ if (NR%2 == 0) { $3="" ; print $1 "_" $2 "/2"} else {print $0} }' $srr'_2.fastq' > temp2.fastq
+
+#Enregistre les nouveaux fichiers à la place de anciens
+mv temp1.fastq  $srr'_1.fastq'
+mv temp2.fastq  $srr'_2.fastq'
 
 done
