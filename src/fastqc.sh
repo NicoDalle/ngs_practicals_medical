b47a45565a08fa84e29f578ba714080f31470ac5
Nicolas Dalle <nicolas.dalle@ens-lyon.fr>
2019-11-06 10:15:29 +0000

    fastqc

--- src/fastqc.sh
+++ src/fastqc.sh
@@ -1,4 +1,19 @@
 #! /bin/bash
 
+# Hsapiens
+data="/home/rstudio/disk"
+mkdir -p $data
+cd $data
+mkdir -p data_qc
 
-fastqc -h
+#Récupére les noms des fichiers à analyser
+FileList=`ls sra_data/`
+
+cd data_qc
+
+#Analyse qualité pour chaque fichier et enregistre les résultats dans data_qc
+for file in $FileList
+do
+echo $file
+fastqc ../sra_data/$file -o .
+done
