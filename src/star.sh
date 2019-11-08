e8d719787cb3d4003bea0cb4e375be00c1ea6747
Nicolas Dalle <nicolas.dalle@ens-yon.fr>
2019-11-08 11:15:23 +0000

    STAR bug

--- src/star.sh
+++ src/star.sh
@@ -13,29 +13,27 @@ SRR3308972
 "
 
 #Génération de l'index du génome humain annoté, avec 7 coeurs
-#STAR --runThreadN 7 --runMode genomeGenerate \
-#  --genomeDir star/index \
-#  --genomeFastaFiles Hsap_genome.fa \
-#  --sjdbGTFfile Hsap_annotation.gtf \
-#  --sjdbOverhang 100
+STAR --runThreadN 7 --runMode genomeGenerate \
+  --genomeDir star/index \
+  --genomeFastaFiles Hsap_genome.fa \
+  --sjdbGTFfile Hsap_annotation.gtf \
+  --sjdbOverhang 100
 
 paired=/home/rstudio/disk/data_trimmed/paired
 
-head $paired/SRR3308952_trimmed_paired_1.fastq
-
-for srr in $SRR :
-do
+#for srr in $SRR :
+#do
 #Création d'un nouveau répertoire
-mkdir star/$srr'_star'
-cd star/$srr'_star'
+#mkdir -p star/$srr'_star'
+#cd star/$srr'_star'
 #Quantification des reads
-STAR --runThreadN 7 --genomeDir /home/rstudio/disk/star/index \
-  --readFilesIn $paired/$srr'_trimmed_paired_1.fastq' \
-  $paired/$srr'_trimmed_paired_2.fastq'
+#STAR --runThreadN 7 --genomeDir /home/rstudio/disk/star/index \
+#  --readFilesIn $paired/$srr'_trimmed_paired_1.fastq' \
+#  $paired/$srr'_trimmed_paired_2.fastq'
   
 #Le fichier Aligned.out.sam est renvoyé par STAR, mais il est trop gros -> conversion en .bam, plus léger
-samtools view -bS -h Aligned.out.sam > $srr'.bam'
+#samtools view -bS -h Aligned.out.sam > $srr'.bam'
 #L'ancien fichier .sam est supprimé
-rm Aligned.out.sam
+#rm Aligned.out.sam
 
-done
+#done
