8e39f71c731bc88936666e027805106c72fe8884
Nicolas Dalle <nicolas.dalle@ens-yon.fr>
2019-11-14 11:00:20 +0000

    STAR avec index Corentin + modif SRR pour tous patients

--- src/star.sh
+++ src/star.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 cd /home/rstudio/disk/
-mkdir -p star/index
+#mkdir -p star/index
 
 #Liste des SRR d'intérêt
 SRR="SRR3308952
@@ -26,29 +26,29 @@ SRR3308972
 
 
 #Génération de l'index du génome humain annoté, avec 7 coeurs
-STAR --runThreadN 7 --runMode genomeGenerate \
-  --genomeDir star/index \
-  --genomeFastaFiles Hsap_chr.fa \
-  --sjdbGTFfile Hsap_annotation.gtf \
-  --sjdbOverhang 100 \
-  --genomeChrBinNbits 18
+#STAR --runThreadN 7 --runMode genomeGenerate \
+#  --genomeDir star/index \
+#  --genomeFastaFiles Hsap_chr.fa \
+#  --sjdbGTFfile Hsap_annotation.gtf \
+#  --sjdbOverhang 100 \
+#  --genomeChrBinNbits 18
 
 
 paired=/home/rstudio/disk/data_trimmed/paired
 
-#for srr in $SRR :
-#do
+for srr in $SRR :
+do
 #Création d'un nouveau répertoire
-#mkdir -p star/$srr'_star'
-#cd star/$srr'_star'
+mkdir -p star/$srr'_star'
+cd star/$srr'_star'
 #Quantification des reads
-#STAR --runThreadN 7 --genomeDir /home/rstudio/disk/star/index \
-#  --readFilesIn $paired/$srr'_trimmed_paired_1.fastq' \
-#  $paired/$srr'_trimmed_paired_2.fastq'
+STAR --runThreadN 7 --genomeDir /home/rstudio/disk/Corentin/hg38_genome \
+  --readFilesIn $paired/$srr'_trimmed_paired_1.fastq' \
+  $paired/$srr'_trimmed_paired_2.fastq'
   
 #Le fichier Aligned.out.sam est renvoyé par STAR, mais il est trop gros -> conversion en .bam, plus léger
-#samtools view -bS -h Aligned.out.sam > $srr'.bam'
+samtools view -bS -h Aligned.out.sam > $srr'.bam'
 #L'ancien fichier .sam est supprimé
-#rm Aligned.out.sam
+rm Aligned.out.sam
 
-#done
+done
