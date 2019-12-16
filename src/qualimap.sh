b6da3a9ddae6c9e7d8c9ab2fc65b369feb798599
Nicolas Dalle <nicolas.dalle@ens-yon.fr>
2019-12-16 11:17:38 +0000

    qualimap pour le tri des bam

--- src/qualimap.sh
+++ src/qualimap.sh
@@ -1,9 +1,41 @@
 #! /bin/bash
 echo "Need to add a dependency : "
-sudo apt-get install libxtst6 -y
+#sudo apt-get install libxtst6 -y
 
-# sort the bam
-samtools
+cd /home/rstudio/disk
+mkdir -p qualimap
 
-qualimap rnaseq 
+#Liste des SRR d'intérêt (attention, on a ici que les SRR pas encore fait)
+SRR="SRR3308952
+SRR3308954
+SRR3308956
+SRR3308963
+SRR3308971
+SRR3308972
+"
+
+for srr in $SRR
+do
+cd /home/rstudio/disk/qualimap
+mkdir -p $srr'_qualimap'
+
+cd /home/rstudio/disk/star/$srr'_star'
+
+#On trie le bam par ordre alphabétique des gènes, et pas selon leur position sur le génome
+#-n : on trie par nom -O : type d'extension qu'on veut -o : output -@ : nombre de threads 
+#dernier argument : fichier input
+samtools sort -n -O bam -o $srr'_sorted.bam' -@ 7 $srr'.bam'
+
+echo $srr
+
+#Il faut trier les bam pour qualimap
+#-bam : fichier bam d'input -gtf : fichier gtf d'annotation du génome -outdir : dossier de sortie
+#-outfile : rapport de sortie -pe : paired end sequencing -s : le bam est trié (obligatoire comme -pe)
+#qualimap rnaseq -bam $srr'_sorted.bam' \
+#  -gtf /home/rstudio/disk/Hsap_annotation.gtf \
+#  -outdir /home/rsudio/disk/qualimap/$srr'_qualimap' \
+#  -outfile /home/rsudio/disk/qualimap/$srr'_qualimap'/$srr'_report.pdf' \
+#  -pe -s
+
+done
 
